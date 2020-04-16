1082b8328239913c2035c77616f625f40eb81e46
Heisenberg <Persey_98@list.ru>
2020-04-16 16:36:07 +0500

    I've begun the project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Homework11_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
