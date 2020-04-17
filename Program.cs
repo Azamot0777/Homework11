b8bfe20e22fea0e885f5b24925da69469ba369da
Heisenberg <Persey_98@list.ru>
2020-04-17 15:45:10 +0500

    I've created methods Insert,Select all and others

--- Program.cs
+++ Program.cs
@@ -6,9 +6,67 @@ namespace Homework11_SQL
 {
     class Program
     {
-        static void Main(string[] args)
+        
+
+        // static void Open ()
+        // {
+        //     if (hh.State == ConnectionState.Close)
+        //     {
+        //         hh.Open ();
+        //     }
+        // }
+        static void Insert (string LastName , string FirstName, string MiddleName,string BirthDate)
+        {
+            const string conString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            SqlConnection hh = new SqlConnection (conString);
+            hh.Open();
+            int y=int.Parse (BirthDate.Substring(0,4)),m=int.Parse(BirthDate.Substring(5,2)),d=int.Parse(BirthDate.Substring(8,2));
+            SqlCommand yy = new SqlCommand ("Insert into Person([LastName],[FirstName],[MiddleName],[BirthDate]) Values ('"+LastName+"','"+FirstName+"','"+MiddleName+"',DATETIMEFROMFORMAT("+y +","+m+","+d+",1,1,1,1))",hh);
+            yy.ExecuteNonQuery();
+        }
+
+        static void SelectAll ()
+        {
+            const string conString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            SqlConnection hh = new SqlConnection (conString);
+            hh.Open();
+            string commandText = "Select * from Person";
+            SqlCommand command = new SqlCommand(commandText, hh);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                System.Console.WriteLine($"Id {reader.GetValue("Id")},LastName:{reader.GetValue("LastName")},FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")},BirthDate:{reader.GetValue("BirthDate")}");
+            }
+            reader.Close();
+        }
+        static void SelectById (string Id)
+        {
+            const string conString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            SqlConnection hh = new SqlConnection (conString);
+            hh.Open();
+            string commandText = "Select * from Person";
+            SqlCommand command = new SqlCommand(commandText, hh);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                if(reader.GetValue(0).ToString() == Id)
+                System.Console.WriteLine($"Id {reader.GetValue("Id")},LastName:{reader.GetValue("LastName")},FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")},BirthDate:{reader.GetValue("BirthDate")}");
+            }
+        }
+        static void Update ()
         {
             
         }
+
+        static void Delete ()
+        {
+            
+        }
+        static void Main(string[] args)
+        {   
+            SelectAll();
+            SelectById("1");
+            Console.ReadKey();
+        }   
     }
 }
