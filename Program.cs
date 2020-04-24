906618b806bb286f67a993ec6eabc00101fc21a2
Heisenberg <Persey_98@list.ru>
2020-04-24 16:21:37 +0500

    I've Created a programm_SQL connection , and switch

--- Program.cs
+++ Program.cs
@@ -2,71 +2,126 @@
 using System.Data;
 using System.Data.SqlClient;
 
-namespace Homework11_SQL
+namespace MyConsoleApp
 {
     class Program
     {
-        
-
-        // static void Open ()
-        // {
-        //     if (hh.State == ConnectionState.Close)
-        //     {
-        //         hh.Open ();
-        //     }
-        // }
-        static void Insert (string LastName , string FirstName, string MiddleName,string BirthDate)
+        static void SelectAll()
         {
-            const string conString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
-            SqlConnection hh = new SqlConnection (conString);
-            hh.Open();
-            int y=int.Parse (BirthDate.Substring(0,4)),m=int.Parse(BirthDate.Substring(5,2)),d=int.Parse(BirthDate.Substring(8,2));
-            SqlCommand yy = new SqlCommand ("Insert into Person([LastName],[FirstName],[MiddleName],[BirthDate]) Values ('"+LastName+"','"+FirstName+"','"+MiddleName+"',DATETIMEFROMFORMAT("+y +","+m+","+d+",1,1,1,1))",hh);
-            yy.ExecuteNonQuery();
-        }
-
-        static void SelectAll ()
-        {
-            const string conString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
-            SqlConnection hh = new SqlConnection (conString);
-            hh.Open();
-            string commandText = "Select * from Person";
-            SqlCommand command = new SqlCommand(commandText, hh);
+            const string connectionString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "SELECT * FROM Person";
+            command.Connection = connection;
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                System.Console.WriteLine($"Id {reader.GetValue("Id")},LastName:{reader.GetValue("LastName")},FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")},BirthDate:{reader.GetValue("BirthDate")}");
+                System.Console.WriteLine($"Id :{reader.GetValue("Id")},\tLastName :{reader.GetValue("LastName")},\tFirstName :{reader.GetValue("FirstName")},\tMiddleName : {reader.GetValue("MiddleName")},\tBirthDate: {reader.GetValue("BirthDate")}");
             }
-            reader.Close();
+            connection.Close();
         }
-        static void SelectById (string Id)
+        static void SelectById(string Id)
         {
-            const string conString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
-            SqlConnection hh = new SqlConnection (conString);
-            hh.Open();
-            string commandText = "Select * from Person";
-            SqlCommand command = new SqlCommand(commandText, hh);
+            const string connectionString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "SELECT * FROM Person";
+            command.Connection = connection;
             SqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
-                if(reader.GetValue(0).ToString() == Id)
-                System.Console.WriteLine($"Id {reader.GetValue("Id")},LastName:{reader.GetValue("LastName")},FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")},BirthDate:{reader.GetValue("BirthDate")}");
+                if (reader.GetValue(0).ToString() == Id)
+                    System.Console.WriteLine($"Id {reader.GetValue("Id")},LastName:{reader.GetValue("LastName")},FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")},BirthDate:{reader.GetValue("BirthDate")}");
             }
-        }
-        static void Update ()
-        {
-            
+            connection.Close();
         }
 
-        static void Delete ()
+        static void Insert()
         {
-            
+            const string connectionString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            string sqlExpression = "INSERT INTO Person (Id,LastName,FirstName,MiddleName,BirthDate) Values(4,'Khaidarov','Azamat','Firdavsi',2001-27-02)";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            SqlCommand command1 = new SqlCommand(sqlExpression, connection);
+            int number = command1.ExecuteNonQuery();
+            Console.WriteLine($"Добавлен Элемент {number}");
+            connection.Close();
         }
-        static void Main(string[] args)
-        {   
-            SelectAll();
-            SelectById("1");
-            Console.ReadKey();
-        }   
+
+        static void Update()
+        {
+            const string connectionString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            string sqlExpression = "UPDATE Person SET BirthDate = 1998-02-27 WHERE LastName = 'Khaidarov'";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            int number = command.ExecuteNonQuery();
+            Console.WriteLine($"Обновлен элемент{number}");
+            connection.Close();
+        }
+
+        static void Delete()
+        {
+            const string connectionString = @"Data source=AZAMOT; Initial catalog=AlifAcademy;Integrated Security = True";
+            string sqlExpression = "DELETE FROM Person WHERE LastName = 'Khaidarov'";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            int number = command.ExecuteNonQuery();
+            Console.WriteLine($"Удаление объекта{number}");
+            connection.Close();
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Adding : 1");
+            Console.WriteLine("Select All : 2");
+            Console.WriteLine("Select by Id : 3");
+            Console.WriteLine("Update : 4");
+            Console.WriteLine("Delete : 5");
+
+            string choise = Console.ReadLine();
+
+            switch (choise)
+            {
+                case 1:
+                    {
+                        Console.Write("Enter Name:");
+                        string Name = Console.ReadLine();
+                        Console.Write("Enter Surname:");
+                        string Surname = Console.ReadLine();
+                        Console.Write("Enter MiddleName:");
+                        string MiddleName = Console.ReadLine();
+                        Insert(Name, Surname, MiddleName);
+
+                    }
+                case 2:
+                    {
+                        SelectAll();
+                    }
+
+                default:
+            }
+
+        }
+
     }
+
 }
+
+
+
+
+
+
+
+
+
